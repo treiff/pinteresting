dfa375a0cb191c24c551ee867236038609893339
treiff <trevorreiff@gmail.com>
2014-05-16 11:57:28 -0400

    added bootstrap and customizations, added team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home' # root_path
   get 'about' => 'pages#about' # Creates about_path
+  get 'team' => 'pages#team' #Creates team_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
