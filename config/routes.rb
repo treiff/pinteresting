3e33a73bc5601fab59e3b3238bcc7ebf17fa44f4
treiff <trevorreiff@gmail.com>
2014-05-18 14:33:54 -0400

    added user database table.  modified header and home to reflect login and logout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home' # root_path
   get 'about' => 'pages#about' # Creates about_path
   get 'team' => 'pages#team' #Creates team_path
