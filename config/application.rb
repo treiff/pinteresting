2b10099668d8a44af5f445b1710cc517f7d137cd
treiff <trevorreiff@gmail.com>
2014-05-26 13:37:29 -0400

    undid changes to application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Pinteresting
     # config.i18n.default_locale = :de
 
     # Required for Heroku
-    # config.assets.initialize_on_precompile = false
+      config.assets.initialize_on_precompile = false
   end
 end
