c68dfa0ca951bb039e035bfddc1d1c4957f55e2b
treiff <trevorreiff@gmail.com>
2014-05-26 20:06:58 -0400

    fixed error in _form.html.erb under f_file_field in div class form group

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+														:default_url => "https://s3.amazonaws.com/pinterestingappomr/ruby.png"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+
 end
