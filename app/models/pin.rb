311e882739ac4041662abe06414a86fc622f6978
treiff <trevorreiff@gmail.com>
2014-05-26 20:30:00 -0400

    added bucket to pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+														:storage => :s3,
+														:bucket => ENV['pinterestingappomr']
 														:default_url => "https://s3.amazonaws.com/pinterestingappomr/SADFACE.jpg"
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
