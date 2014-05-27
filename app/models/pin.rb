25829ccada8cd3b3585220b2748039578dcc8035
treiff <trevorreiff@gmail.com>
2014-05-26 21:39:44 -0400

    fixed syntax error from last push

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	# :bucket => 'pinterestingappomr'
 	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :image, presence: true
