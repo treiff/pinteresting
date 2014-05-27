b6b13c1534b9513dcdf39dc97226ee0615028707
treiff <trevorreiff@gmail.com>
2014-05-26 21:44:41 -0400

    put pin model back to where it should be

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	# :bucket => 'pinterestingappomr'
-	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+	:bucket => 'pinterestingappomr'
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :image, presence: true
 	validates :description, presence: true
 end
