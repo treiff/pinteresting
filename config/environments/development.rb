f37c4c15e005a06f1cfbcadaa6b8ea23f8737672
treiff <trevorreiff@gmail.com>
2014-05-27 14:35:35 -0400

    added turbolinks and masonary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,13 +37,4 @@ Rails.application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
 end
