c3a6f0e5f804fb499bc38a0bb04d4d2f6e100b9b
treiff <trevorreiff@gmail.com>
2014-05-27 15:58:00 -0400

    added home partial, and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root 'pages#home' # root_path
+  root 'pins#index' # root_path
   get 'about' => 'pages#about' # Creates about_path
   get 'team' => 'pages#team' #Creates team_path
 
